27aeaa7b31430d8b04675ce5ae85efc04cfa465e
cursos-programacao <mendes.marques.andre@gmail.com>
2024-06-04 16:07:57 +0000

    Atualização do ficheiro CilindroTest.java

--- tests/CilindroTest.java
+++ tests/CilindroTest.java
@@ -2,18 +2,17 @@ package tests;
 import org.junit.jupiter.api.*;
 import classes.Cilindro;
 
-public class CilindroTest 
-{
-    
-    private Cilindro cilindro;
-    final double pi = 3.14;
-    final double raio = 6;
-    final double altura = 5;
+public class CilindroTest {
 
-    /* Este método é executado antes de cada teste */
+    // Declaração das variáveis utilizadas nos testes
+    private Cilindro cilindro;
+    private double raio;
+    private double altura;
+    final double pi = 3.1416;
+
     @BeforeEach
     public void setUp() {
-        cilindro = new Cilindro();
+        cilindro = new Cilindro(5, 10);
     }
 
-}   
+}
