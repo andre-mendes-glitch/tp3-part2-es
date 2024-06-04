4dae8531651a2a95ade7e6c7e7d32762bcd6107b
cursos-programacao <mendes.marques.andre@gmail.com>
2024-06-04 14:10:54 +0000

    Remoção de código desnecessário do ficheiro 'CilindroTest.java'

--- tests/CilindroTest.java
+++ tests/CilindroTest.java
@@ -16,40 +16,4 @@ public class CilindroTest
         cilindro = new Cilindro();
     }
 
-    /*
-     * Este teste serve para verificar o que acontece quando o nome do objeto é definido corretamente.
-     */
-    @Test
-    public void testCorrectNameObject() 
-    {
-        
-        // Atribui o nome esperado a uma variável
-        String nomeEsperado = "Cilindro";
-
-        // Atribui o nome obtido a uma variável a partir do método getNome()
-        String nomeObtido = cilindro.getNome();
-
-        // Verifica se o nome obtido é igual ao nome esperado
-        Assertions.assertEquals(nomeEsperado, nomeObtido);
-
-    }
-    
-    /*
-     * Este teste serve para verificar o que acontece quando o nome do objeto é definido incorretamente.
-     */
-    @Test
-    public void testIncorrectNameObject() 
-    {
-
-        // Atribui o nome esperado a uma variável
-        String nomeEsperado = "Cilindro";
-
-        // Atribui um nome incorreto à variável nomeObtido
-        String nomeObtido = "Esfera";
-
-        // Verifica se o nome obtido é igual ao nome esperado
-        Assertions.assertEquals(nomeEsperado, nomeObtido, "O nome do objeto tem que ser 'Cilindro'");
-
-    }
-
 }   
