1104669ca743f52a897187ec288473d86c208473
cursos-programacao <mendes.marques.andre@gmail.com>
2024-06-07 16:23:14 +0000

    Atualização da classe 'FigurasPlanas' e criação da interface 'Perimetro'.

--- classes/FigurasPlanas.java
+++ classes/FigurasPlanas.java
@@ -1,6 +1,6 @@
 package classes;
 
-public abstract class FigurasPlanas extends Figuras implements Area {
+public abstract class FigurasPlanas extends Figuras implements Area, Perimetro {
 
     @Override
     public String toString() {
---
+++ classes/Perimetro.java
@@ -0,0 +1,7 @@
+package classes;
+
+public interface Perimetro {
+
+    double calcularPerimetro();
+
+}
